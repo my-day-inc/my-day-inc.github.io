cc191713130a74d226297fc4e869609b2748d24f
Artjom Löbsack <ceigh@pm.me>
2020-05-13 17:00:02 +0500

    :sparkles: Add contacts cards

---
+++ components/Cards/ContactsCard.vue
@@ -0,0 +1,66 @@
+<template lang='pug'>
+el-card
+  .clearfix(slot='header')
+    span {{ name }}
+    el-button(v-if='btnTxt' type='danger' @click='btnCb') {{ btnTxt }}
+
+  .list-item(v-if='data.phone')
+    i.el-icon-phone
+    el-link(type='info'
+            :href='`tel:${data.phone}`'
+            target='_blank') {{ data.phone }}
+  .list-item(v-if='data.email')
+    i.el-icon-s-custom
+    el-link(type='info'
+            :href='`mailto:${data.email}`'
+            target='_blank') {{ data.email }}
+</template>
+
+<script lang='ts'>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    btnTxt: {
+      type: String,
+      default: null
+    },
+    btnCb: {
+      type: Function,
+      default: () => {}
+    },
+    data: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
+
+<style lang='sass' scoped>
+  .el-card
+    margin-bottom: 2rem
+
+    &:last-child
+      margin-bottom: 0
+
+  .clearfix
+    font-weight: 700
+
+  .el-button
+    float: right
+    padding: 5px
+    font-weight: 700
+
+  .list-item
+    font-size: 14px
+    margin-bottom: 18px
+
+    i
+      font-size: 1rem
+
+    .el-link
+      margin-left: 10px
+</style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,3 +1,70 @@
 <template lang='pug'>
-  h1 Планировщик
+  div
+    h1 Планировщик
+
+    el-row.cols(type='flex'
+                justify='space-between'
+                :gutter='32')
+      el-col
+        el-container(direction='vertical')
+          h3 Контакты
+          ContactsCard(v-for='(c, i) in contacts'
+                       :key='i'
+                       :name='c.name'
+                       :data='c.data'
+                       btn-txt='Удалить')
+
+      el-col
+        el-container(direction='vertical')
+          h3 Неделя
+
+      el-col
+        el-container(direction='vertical')
+          h3 Месяц
 </template>
+
+<script lang='ts'>
+import ContactsCard from '~/components/Cards/ContactsCard.vue'
+
+export default {
+  components: {
+    ContactsCard
+  },
+
+  data () {
+    return {
+      contacts: [
+        {
+          name: 'Костик Хыпищев',
+          data: {
+            phone: '+7 (074) 153 49-94',
+            email: 'kostya@mail.su'
+          }
+        },
+        {
+          name: 'Натка Верепеева',
+          data: {
+            phone: '+7 (074) 153 49-94',
+            email: 'nata@yahoo.eu'
+          }
+        },
+        {
+          name: 'Витя Гершев',
+          data: {
+            phone: '+7 (074) 153 49-94',
+            email: 'v1t3k@gmail.com'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='sass' scoped>
+  h3
+    margin-bottom: 2rem
+
+  .cols
+    margin-top: 3rem
+</style>
