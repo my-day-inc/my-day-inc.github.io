c6f0759d241ccbf14cc54478cccab771efb790eb
ceigh <ceigh@users.noreply.github.com>
2020-05-15 00:50:52 +0000

    deploy: b56808cc529e42ac46e2d75d0be047337eed1bb2

---
+++ _nuxt/3cea4d7a027211edd045.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{384:function(n,t,e){"use strict";e.r(t);var l=e(20),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("h1",[this._v("Настройки")])}),[],!1,null,null,null);t.default=component.exports}}]);
