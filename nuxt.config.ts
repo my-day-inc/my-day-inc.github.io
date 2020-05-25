5c4cebaf87a46e23f65f350a6f56a4e019908d09
Artjom Löbsack <ceigh@pm.me>
2020-05-25 15:32:06 +0500

    :sparkles: Set nuxt-ts runtime

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Социальный дневник' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -93,10 +93,13 @@ export default {
       /^element-ui/,
       /typed-vuex/
     ],
+    parallel: true
     /*
     ** You can extend webpack config here
     */
+    /*
     extend (_config, _ctx) {
     }
+    */
   }
 }
